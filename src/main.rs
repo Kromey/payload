eb38e52932df4984491e9809e7c90d2b57ebabcb
Travis Veazey <travisvz@gmail.com>
2019-03-01 11:03:19 -0900

    Use "unseeded" PRNG for dice rolls

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
 
     // Let's roll some dice!!
     let mut results = [0,0,0,0,0,0,0,0,0,0];
+    let mut xo = rand::Xoshiro256::new();
     for _x in 0..1_000_000 {
         let n = xo.nroll(2, 4);
         results[n as usize] += 1;
