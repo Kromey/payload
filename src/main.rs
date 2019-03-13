cebb42b71f6edcff2c91151d4c79953e1be84b31
Travis Veazey <travisvz@gmail.com>
2019-03-12 16:02:56 -0800

    Getting Amethyst initialized

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-mod rand;
+extern crate amethyst;
 
-fn main() {
-    let seed = 1880u64;
+use amethyst::{
+    prelude::*,
+    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    utils::application_root_dir,
+};
 
-    let mut xo = rand::Rand::from_seed(seed);
-    println!("{:?}", xo);
-    for _x in 0..100 {
-        let n = xo.rand_u64();
-        println!("Random: {:?}", n);
-    }
+struct Example;
 
-    // Let's roll some dice!!
-    let mut results = [0,0,0,0,0,0,0,0,0,0];
-    let mut xo = rand::Rand::new();
-    for _x in 0..1_000_000 {
-        let n = xo.roll_ndx(2, 4);
-        results[n as usize] += 1;
-    }
-    println!("{:?}", results);
+impl SimpleState for Example {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let path = format!(
+        "{}/resources/display_config.ron",
+        application_root_dir()
+    );
+    let config = DisplayConfig::load(&path);
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosNormTex>::new()),
+    );
+
+    let game_data =
+        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let mut game = Application::new("./", Example, game_data)?;
+
+    game.run();
+
+    Ok(())
 }
