7f2ad8b9d10523fb619f6dd07174ea12693b611b
Travis Veazey <travisvz@gmail.com>
2023-10-31 11:03:02 -0800

    Toss out optimization, build fov with lots of rays
    Instead of optimizing the view cone by only casting rays at corners, we
    now simply throw out a bunch of rays at regular increments throughout
    the view cone and collect all of those points. While the result is a lot
    more rays, we no longer have the problem of trying to find arc-ray
    intersections to complete the cone, nor do we have to do any "funny
    business" to give the cone a curve.
    
    The code is now a lot simpler, and we can always come back IFF we need
    to optimize this

--- src/fov.rs
+++ src/fov.rs
@@ -5,102 +5,77 @@ use bevy_rapier2d::prelude::*;
 
 use crate::{core::OPAQUE_GROUP, player::Player};
 
-/// How wide to cast auxiliary rays when checking for vision past a corner
-///
-/// When a corner is seen within a view cone, move ±this distance from the
-/// corner, perpendicular to the ray, and cast new rays to determine viewable
-/// distance past the corner.
-const AUXILIARY_RAY_DISTANCE: f32 = 1.75;
+/// How much to rotate each ray when calculating view cone
+const RAY_ROTATION_ANGLE: f32 = TAU / 360.0;
+
+/// FIXME: These should be debug options that can be switched on/off at runtime
+const DEBUG_RAYS: bool = false;
+const DEBUG_VIEW_CONE: bool = true;
+const DRAW_MAXIMAL_VIEW_CONE: bool = true;
 
 pub fn calculate_fov(
     rapier_context: Res<RapierContext>,
     player_qry: Query<(Entity, &GlobalTransform), With<Player>>,
     mut gizmos: Gizmos,
 ) {
-    //FIXME: These points will exist in the map and should be queried from there
-    let x1 = -32.0;
-    let x2 = 32.0;
-    let y1 = 128.0 - 16.0;
-    let y2 = 128.0 + 16.0;
-
-    let points = [
-        Vec2::new(x1, y1),
-        Vec2::new(x1, y2),
-        Vec2::new(x2, y1),
-        Vec2::new(x2, y2),
-    ];
-
     let maximal_color = Color::GRAY;
-    let viewable_color = Color::NAVY;
-    let visible_corner_color = Color::RED;
+    let ray_color = Color::NAVY;
+    let viewable_color = Color::PURPLE;
 
     let max_toi = 256.0;
     let solid = true;
     let filter = QueryFilter::new().groups(CollisionGroups::new(Group::all(), OPAQUE_GROUP));
     let view_cone = TAU / 12.0; // Vision only extends ±this angle
+    let num_rays = (view_cone * 2.0 / RAY_ROTATION_ANGLE).floor() as usize;
 
     for (player, player_transform) in player_qry.iter() {
         let player_facing = player_transform.right().truncate();
         let player_pos = player_transform.translation().truncate();
         let filter = filter.exclude_collider(player);
 
-        gizmos.arc_2d(
-            player_pos,
-            player_facing.angle_between(Vec2::Y),
-            view_cone * 2.0,
-            max_toi,
-            maximal_color,
-        );
+        let mut ray = Vec2::from_angle(-view_cone).rotate(player_facing);
 
-        for &corner in points.iter() {
-            let ray_dir = (corner - player_pos).normalize();
-            if player_facing.angle_between(ray_dir).abs() <= view_cone {
-                gizmos.line_2d(player_pos, corner, maximal_color);
-                if let Some((_, toi)) =
-                    rapier_context.cast_ray(player_pos, ray_dir, max_toi, solid, filter)
-                {
-                    let contact = player_pos + ray_dir * toi;
-                    gizmos.line_2d(player_pos, contact, viewable_color);
-
-                    // If we reached the corner, cast auxiliary rays
-                    // See Section 2.2 here https://legends2k.github.io/2d-fov/design.html for an optimization,
-                    // but it requires knowing the line segments, not just the list of corners
-                    if contact.distance_squared(corner) < 1.0 {
-                        gizmos.circle_2d(contact, 1.0, visible_corner_color);
-
-                        // Since `ray_dir` is already normalized, the perpendicular vec is also normalized
-                        let perpendicular_step = ray_dir.perp() * AUXILIARY_RAY_DISTANCE;
-
-                        for aux_ray_dir in [
-                            (corner - perpendicular_step - player_pos).normalize(),
-                            (corner + perpendicular_step - player_pos).normalize(),
-                        ] {
-                            if rapier_context
-                                .cast_ray(player_pos, aux_ray_dir, max_toi, solid, filter)
-                                // TODO: With additional objects, we may see past to them - not just to end of the cone
-                                .is_none()
-                            {
-                                let contact = player_pos + aux_ray_dir * max_toi;
-                                gizmos.line_2d(player_pos, contact, Color::PURPLE);
-
-                                // We've seen past this corner already, no reason to try the other side
-                                break;
-                            }
-                        }
-                    }
-                }
+        if DRAW_MAXIMAL_VIEW_CONE {
+            // ## Draw view cone outline ##
+            gizmos.arc_2d(
+                player_pos,
+                player_facing.angle_between(Vec2::Y),
+                view_cone * 2.0,
+                max_toi,
+                maximal_color,
+            );
+            for angle in [Vec2::from_angle(view_cone), Vec2::from_angle(-view_cone)] {
+                let ray_dir = player_facing.rotate(angle);
+                gizmos.ray_2d(player_pos, ray_dir * max_toi, maximal_color);
+                // This is the edge of the view cone, always draw this ray even if it doesn't collide with anything
+                let toi = rapier_context
+                    .cast_ray(player_pos, ray_dir, max_toi, solid, filter)
+                    .map(|(_, toi)| toi)
+                    .unwrap_or(max_toi);
+                gizmos.ray_2d(player_pos, ray_dir * toi, maximal_color);
             }
         }
 
-        for angle in [Vec2::from_angle(view_cone), Vec2::from_angle(-view_cone)] {
-            let ray_dir = player_facing.rotate(angle);
-            gizmos.ray_2d(player_pos, ray_dir * max_toi, maximal_color);
-            // This is the edge of the view cone, always draw this ray even if it doesn't collide with anything
+        // Iterate inclusively to ensure we draw both edges of our view cone
+        // OPTIMIZATION: We could collect corners and only cast rays to those, and then calculate the intersections
+        // of the arc with each line segment and cast rays to those, to cast a lot fewer rays
+        let mut points = vec![player_pos];
+        points.extend((0..=num_rays).map(|_| {
             let toi = rapier_context
-                .cast_ray(player_pos, ray_dir, max_toi, solid, filter)
+                .cast_ray(player_pos, ray, max_toi, solid, filter)
                 .map(|(_, toi)| toi)
                 .unwrap_or(max_toi);
-            gizmos.ray_2d(player_pos, ray_dir * toi, viewable_color);
+            let end = player_pos + ray * toi;
+            if DEBUG_RAYS {
+                gizmos.line_2d(player_pos, end, ray_color);
+            }
+            ray = ray.rotate(Vec2::from_angle(RAY_ROTATION_ANGLE));
+
+            end
+        }));
+        points.push(player_pos);
+        if DEBUG_VIEW_CONE {
+            gizmos.linestrip_2d(points, viewable_color);
         }
     }
 }
