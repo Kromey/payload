e103400caa7bb7211e51ebce0bae28ac01dc2a1e
Travis Veazey <travisvz@gmail.com>
2023-11-01 13:42:17 -0800

    Reveal background only as the player sees it

--- src/player.rs
+++ src/player.rs
@@ -63,12 +63,15 @@ pub fn spawn_player(
     // This will be the mesh for the player's field of view
     let mesh_handle = meshes.add(Mesh::new(PrimitiveTopology::TriangleList));
     let render_target = images.add(image);
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: mesh_handle.clone().into(),
-        material: materials.add(ColorMaterial::from(render_target.clone())),
-        transform: Transform::from_xyz(0.0, 0.0, 200.0),
-        ..Default::default()
-    });
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.clone().into(),
+            material: materials.add(ColorMaterial::from(render_target.clone())),
+            transform: Transform::from_xyz(0.0, 0.0, 200.0),
+            ..Default::default()
+        },
+        RenderLayers::default().with(2),
+    ));
     commands.spawn((
         SpriteBundle {
             texture: sprites.player.clone(),
@@ -161,7 +164,7 @@ pub fn spawn_player(
             texture: asset_server.load("nebula.png"),
             ..Default::default()
         },
-        RenderLayers::default().with(1),
+        RenderLayers::layer(1), // Layer 1 is where the fov camera looks; hides this from the default camera
     ));
     // Overlay a "fog of war"
     commands.spawn(SpriteBundle {
@@ -173,6 +176,54 @@ pub fn spawn_player(
         transform: Transform::from_xyz(0.0, 0.0, 100.0),
         ..Default::default()
     });
+
+    // Spawn an empty texture we'll draw the "explored" map to
+    let size = Extent3d {
+        width: 2048,
+        height: 2048,
+        ..Default::default()
+    };
+    // This is the texture that the seen map will be rendered to.
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Bgra8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..Default::default()
+    };
+    image.resize(size);
+    let render_target = images.add(image);
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 1.0),
+            texture: render_target.clone(),
+            ..Default::default()
+        },
+        RenderLayers::default().with(2), // layers 0 and 2 make it visible only to default and "explorer" cameras
+    ));
+    // Spawn a camera that will be used to reveal the explored map
+    commands.spawn((
+        Camera2dBundle {
+            camera_2d: Camera2d {
+                clear_color: ClearColorConfig::Custom(Color::BLACK),
+            },
+            camera: Camera {
+                order: -2,
+                target: RenderTarget::Image(render_target),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        RenderLayers::layer(2),
+    ));
 }
 
 pub fn player_debug(player_qry: Query<&GlobalTransform, With<Player>>, mut gizmos: Gizmos) {
