41c1bf8375e5f144aa671c5d0d3c07a8a586103f
Travis Veazey <travisvz@gmail.com>
2023-11-09 14:32:54 -0900

    Silence warning for now on unused type

--- src/rand.rs
+++ src/rand.rs
@@ -5,7 +5,7 @@ type RngAlgorithm = WyRand;
 
 pub type RandPlugin = EntropyPlugin<RngAlgorithm>;
 pub type WorldRng = GlobalEntropy<RngAlgorithm>;
-pub type RngComponent = EntropyComponent<RngAlgorithm>;
+pub type _RngComponent = EntropyComponent<RngAlgorithm>;
 
 pub fn world_seed<S: std::hash::Hash>(seed: S) -> WorldRng {
     rand_seeder::Seeder::from(seed).make_rng()
